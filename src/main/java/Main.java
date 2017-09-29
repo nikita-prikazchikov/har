21dd72cb9fc2d59157a5b8ae1a2a779524016d57
Nikita Prikazchikov <nprikazchikov@griddynamics.com>
2017-09-29 20:24:17 +0400

    add autoreplace

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -17,6 +18,12 @@ public class Main {
 
     private static String RESULTS_DIRECTORY = "results";
     private static String RESULTS_DIRECTORY_ALL = "results/all";
+    private static HashMap<String, String> replaces = new HashMap<String, String>(10);
+
+    private static String TEST_USER_ID = "Virt8gYfcGynEBUXKj";
+    private static String TEST_ACCESS_TOKEN = "svGUAutomation0001.svJcp";
+    private static String TEST_REFRESH_TOKEN = "svGUAutomation0001.svJcp.Rt0001";
+    private static String TEST_DOMAIN = "services.matrix.dp-dev.jcpcloud2.net";
 
     public static void main(String[] args) throws HarReaderException, IOException {
         String HAR_DIRECTORY = "hars";
@@ -41,6 +48,7 @@ public class Main {
             return;
         }
         for (File file:files){
+            replaces.clear();
             if (file.getPath().endsWith(".har")) {
                 processFile(file);
             }
@@ -61,6 +69,12 @@ public class Main {
         createDirectory(new File(directoryPath));
 
         if (new File(directoryPath).exists()) {
+            //Initial quick run to identify fields for replacement
+            for (HarEntry jsonEntry : jsonEntries) {
+                collectReplaces(jsonEntry);
+            }
+            System.out.println("Replaces will be done:" + replaces.toString());
+
             for (HarEntry jsonEntry : jsonEntries) {
                 harEntityToJson(jsonEntry, directoryPath);
             }
@@ -95,6 +109,34 @@ public class Main {
         return entries;
     }
 
+    private static void collectReplaces(HarEntry jsonEntry){
+        for (HarHeader harHeader : jsonEntry.getResponse().getHeaders()){
+            if(harHeader.getName().equalsIgnoreCase("DP_SFL_ID")){
+                replaces.put(harHeader.getValue(), TEST_USER_ID);
+            }
+            if(harHeader.getName().equalsIgnoreCase("Access_Token")){
+                replaces.put(harHeader.getValue(), TEST_ACCESS_TOKEN);
+            }
+            if(harHeader.getName().equalsIgnoreCase("Refresh_Token")){
+                replaces.put(harHeader.getValue(), TEST_REFRESH_TOKEN);
+            }
+        }
+        if(jsonEntry.getResponse().getContent().getText()!=null) {
+            Pattern pattern = Pattern.compile("refresh_token\\W+([-\\w\\.]+)");
+            Matcher matcher = pattern.matcher(jsonEntry.getResponse().getContent().getText());
+            if (matcher.find()) {
+                replaces.put(matcher.group(1), TEST_REFRESH_TOKEN);
+            }
+
+            pattern = Pattern.compile("access_token\\W+([-\\w\\.]+)");
+            matcher = pattern.matcher(jsonEntry.getResponse().getContent().getText());
+
+            if (matcher.find()) {
+                replaces.put(matcher.group(1), TEST_ACCESS_TOKEN);
+            }
+        }
+    }
+
     private static void harEntityToJson(HarEntry jsonEntry, String directoryPath)
             throws IOException {
         ObjectMapper mapper = new ObjectMapper();
@@ -103,7 +145,8 @@ public class Main {
         ArrayNode responses = objectNode.putArray("responses");
         ObjectNode is = responses.addObject().putObject("is");
         predicates.addObject().putObject("deepEquals")
-                .put("path", getRelativePath(jsonEntry.getRequest().getUrl()));
+                .put("path", replaceData(getRelativePath(jsonEntry.getRequest().getUrl())));
+
         if (jsonEntry.getRequest().getQueryString() != null) {
             ObjectNode query = predicates.addObject().putObject("deepEquals")
                     .putObject("query");
@@ -111,13 +154,12 @@ public class Main {
                 query.put(harQueryParam.getName(), harQueryParam.getValue());
             }
         }
-        if (jsonEntry.getRequest().getBodySize() == 0) {
-            predicates.addObject().putObject("deepEquals").put("body", "");
-        } else {
+
+        if (jsonEntry.getRequest().getBodySize() != 0) {
             predicates.addObject().putObject("deepEquals")
-                    .put("body",
-                            jsonEntry.getRequest().getPostData().getText());
+                    .put("body", replaceData(jsonEntry.getRequest().getPostData().getText()));
         }
+
         predicates.addObject().putObject("deepEquals")
                 .put("method",
                         jsonEntry.getRequest().getMethod().name());
@@ -127,22 +169,42 @@ public class Main {
         ArrayNode setCookie = headers.putArray("Set-Cookie");
         for (HarHeader harHeader : jsonEntry.getResponse().getHeaders()) {
             if (harHeader.getName().equals("Set-Cookie")) {
-                setCookie.add(harHeader.getValue());
+                setCookie.add(replaceData(harHeader.getValue()));
             }
         }
         for (HarHeader harHeader : jsonEntry.getResponse().getHeaders()) {
             if (!harHeader.getName().equals("Set-Cookie")) {
-                headers.put(harHeader.getName(), harHeader.getValue());
+                headers.put(harHeader.getName(), replaceData(harHeader.getValue()));
             }
         }
         headers.put("X-PROXY", "Service Virtualization");
+
         if (jsonEntry.getResponse().getContent().getText() != null) {
-            is.put("body", jsonEntry.getResponse().getContent().getText());
+            is.put("body", replaceData(jsonEntry.getResponse().getContent().getText()));
         }
+
         is.put("_mode", "text");
+
 
         printJsonToFile(directoryPath, jsonEntry, mapper, objectNode);
         printJsonToFile(RESULTS_DIRECTORY_ALL, jsonEntry, mapper, objectNode);
+    }
+
+    private static String replaceData(String inputString){
+        String result = inputString;
+        for(String key : replaces.keySet()){
+            result = result.replaceAll(key, replaces.get(key));
+        }
+        return replaceAccountHref(result);
+    }
+
+    private static String replaceAccountHref(String inputString){
+        Pattern pattern = Pattern.compile("account_href\\W+https://([^/]+)");
+        Matcher matcher = pattern.matcher(inputString);
+        if (matcher.find()) {
+            return inputString.replaceAll(matcher.group(1), TEST_DOMAIN);
+        }
+        return inputString;
     }
 
     private static void printJsonToFile(String directoryPath,
