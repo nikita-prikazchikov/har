8a12fa4955dee292b14ff3f9e1e2beb083ceced6
Nikita Prikazchikov <nprikazchikov@griddynamics.com>
2017-09-29 18:38:09 +0400

    Update parsing to all files in folder. Add all folder for combined results

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,41 +9,93 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
 
-    public static void main(String[] args) throws HarReaderException, IOException {
-        System.out.println("Enter path to .har file");
-        Scanner scanner = new Scanner(System.in);
-        String path = scanner.nextLine();
-        HarReader harReader = new HarReader();
-        Har har = harReader.readFromFile(new File(path)); //jcpenney.com.har
-        ArrayList<HarEntry> jsonEntries = new ArrayList<HarEntry>();
+    private static String RESULTS_DIRECTORY = "results";
+    private static String RESULTS_DIRECTORY_ALL = "results/all";
 
-        for (HarEntry arg : har.getLog().getEntries()) {
-            if ((arg.getResponse().getContent().getMimeType().equals("application/json")
-                    || arg.getRequest().getMethod() == HttpMethod.POST
-                    || arg.getResponse().getContent().getMimeType().equals("text/json"))
-                    && arg.getRequest().getUrl().contains("jcpenney")
-                    ||arg.getRequest().getUrl().contains("api.jcpenney")) {
-                jsonEntries.add(arg);
+    public static void main(String[] args) throws HarReaderException, IOException {
+        String HAR_DIRECTORY = "hars";
+
+        File results = new File(RESULTS_DIRECTORY);
+        createDirectory(results);
+
+        File resultsAll = new File(RESULTS_DIRECTORY_ALL);
+        createDirectory(resultsAll);
+
+        File harDirectory = new File(HAR_DIRECTORY);
+        if(!harDirectory.exists()){
+            System.out.println("Har directory is missing: " + HAR_DIRECTORY);
+            createDirectory(harDirectory);
+            System.out.println("Put your har files into: " + HAR_DIRECTORY);
+            return;
+        }
+
+        File[] files = harDirectory.listFiles();
+        if (files == null){
+            System.out.println("Har files are not found in directory: " + HAR_DIRECTORY);
+            return;
+        }
+        for (File file:files){
+            if (file.getPath().endsWith(".har")) {
+                processFile(file);
             }
         }
-        String directoryPath = path.replaceAll("//", "-").replaceAll("\\.", "");
-        boolean isDirectoryCreated = new File(directoryPath).mkdir();
-        System.out.println(
-                "Directory " + directoryPath + " was created : " + isDirectoryCreated);
-        if (isDirectoryCreated || new File(directoryPath).exists()) {
+    }
+
+    private static void processFile(File file) throws HarReaderException, IOException {
+        System.out.println("Process file: " + file.getPath());
+        if (!file.exists()) {
+            System.out.println("File does not exist: " + file.getPath());
+            return;
+        }
+        HarReader harReader = new HarReader();
+        Har har = harReader.readFromFile(file);
+        ArrayList<HarEntry> jsonEntries = filterEntries(har.getLog().getEntries());
+
+        String directoryPath = RESULTS_DIRECTORY + "/" + file.getName().replaceAll(".har", "");
+        createDirectory(new File(directoryPath));
+
+        if (new File(directoryPath).exists()) {
             for (HarEntry jsonEntry : jsonEntries) {
                 harEntityToJson(jsonEntry, directoryPath);
             }
         }
     }
 
-    public static void harEntityToJson(HarEntry jsonEntry, String directoryPath)
+    private static void createDirectory(File directory){
+        if (!directory.exists()){
+            if (directory.mkdir()){
+                System.out.println("Directory \"" + directory.getPath() + "\" was created.");
+            }
+        }
+    }
+
+    private static ArrayList<HarEntry> filterEntries(List<HarEntry> harEntries) {
+        ArrayList<HarEntry> entries = new ArrayList<HarEntry>();
+        for (HarEntry arg : harEntries) {
+            if (
+                    (
+                            (
+                                    arg.getResponse().getContent().getMimeType().equals("application/json")
+                                            || arg.getResponse().getContent().getMimeType().equals("text/json")
+                                            || arg.getRequest().getMethod() == HttpMethod.POST
+                            )
+                                    && arg.getRequest().getUrl().matches("^https?://[^/]+jcpenney.*")
+                    )
+                            || arg.getRequest().getUrl().matches("^https?://[^/]+api\\.jcpenney.*")) {
+                entries.add(arg);
+            }
+        }
+        System.out.println(String.format("There are %d request filtered from %d", entries.size(), harEntries.size()));
+        return entries;
+    }
+
+    private static void harEntityToJson(HarEntry jsonEntry, String directoryPath)
             throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         ObjectNode objectNode = mapper.createObjectNode();
@@ -83,16 +135,18 @@ public class Main {
                 headers.put(harHeader.getName(), harHeader.getValue());
             }
         }
+        headers.put("X-PROXY", "Service Virtualization");
         if (jsonEntry.getResponse().getContent().getText() != null) {
             is.put("body", jsonEntry.getResponse().getContent().getText());
         }
         is.put("_mode", "text");
 
         printJsonToFile(directoryPath, jsonEntry, mapper, objectNode);
+        printJsonToFile(RESULTS_DIRECTORY_ALL, jsonEntry, mapper, objectNode);
     }
 
-    public static void printJsonToFile(String directoryPath,
-            HarEntry jsonEntry, ObjectMapper mapper, Object objectNode) throws IOException {
+    private static void printJsonToFile(String directoryPath,
+                                        HarEntry jsonEntry, ObjectMapper mapper, Object objectNode) throws IOException {
 
         String folder = "";
         Pattern pattern = Pattern.compile("https?://([^/]+)");
@@ -100,9 +154,7 @@ public class Main {
         if (matcher.find()){
             folder = "/" + matcher.group(1);
             File dir = new File(directoryPath + folder);
-            if (!dir.exists()){
-                dir.mkdir();
-            }
+            createDirectory(dir);
         }
 
         String jsonPath = directoryPath + folder + "/" + jsonEntry.getRequest().getMethod() + getRelativePath(
@@ -123,6 +175,13 @@ public class Main {
     }
 
     private static String getRelativePath(String absolutePath) {
+        Pattern pattern = Pattern.compile("https?://(?:[^/]+)([^?]*)");
+        Matcher matcher = pattern.matcher(absolutePath);
+
+        if(matcher.find()){
+            return matcher.group(1);
+        }
+
         StringBuilder stringBuilder = new StringBuilder();
         String[] strings = absolutePath.split("/");
         for (int i = 3; i < strings.length - 1; i++) {
